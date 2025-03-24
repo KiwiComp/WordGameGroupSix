2c933be92971e04d5558ea139ee37f702d181f31
KiwiComp <kim.bellander@gmail.com>
2025-03-24 15:28:45 +0100

    Initial Commit

---
+++ WordGameGroupSix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordGameGroupSix
+//
+//  Created by KiwiComp on 2025-03-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
